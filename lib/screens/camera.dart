ff39c501e0c1894b75fa44d878d80f4b980c10a1
Lakshay Baweja <lakshay17csu093@ncuindia.edu>
2020-06-09 12:59:17 +0530

    Camera demo completed

---
+++ lib/screens/camera.dart
@@ -0,0 +1,73 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class CameraDemo extends StatefulWidget {
+  @override
+  _CameraDemoState createState() => _CameraDemoState();
+}
+
+class _CameraDemoState extends State<CameraDemo> {
+  File image;
+  openCamera() async {
+    File image = await ImagePicker.pickImage(source: ImageSource.camera);
+    setState(() {
+      this.image = image;
+    });
+  }
+
+  openGallery() async {
+    File image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      this.image = image;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var media = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Camera'),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: Container(
+        margin: EdgeInsets.only(left: 120),
+        child: Column(
+//          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          children: <Widget>[
+            RaisedButton(
+              child: Text('Camera Open'),
+              onPressed: () {
+                openCamera();
+              },
+            ),
+            RaisedButton(
+              child: Text('Gallery Open'),
+              onPressed: () {
+                openGallery();
+              },
+            ),
+            image == null
+                ? Container(
+                    child: Text(
+                      'No Image',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                  )
+                : Image.file(
+                    image,
+                    height: media.height * 0.40,
+                    width: media.width * 0.40,
+                  )
+          ],
+        ),
+      ),
+    );
+  }
+}
